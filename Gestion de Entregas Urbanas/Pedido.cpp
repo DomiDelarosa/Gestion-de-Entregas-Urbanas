bf2b062f2d3cc1ed1cdd129d7e43cd6ad0338aca
Dominick <135394582+Tarshed@users.noreply.github.com>
2025-12-04 00:50:13 -0500

    Desarrollar clase Pedido

--- Gestion de Entregas Urbanas/Pedido.cpp
+++ Gestion de Entregas Urbanas/Pedido.cpp
@@ -1,1 +1,49 @@
 #include "Pedido.h"
+
+char Pedido::getId() {
+	return id;
+}
+int Pedido::getVolumen() {
+	return volumen;
+}
+int Pedido::getPrioridad() {
+	return prioridad;
+}
+int Pedido::getOrigenX() {
+	return origenX;
+}
+int Pedido::getOrigenY() {
+	return origenY;
+}
+int Pedido::getDestinoX() {
+	return destinoX;
+}
+int Pedido::getDestinoY() {
+	return destinoY;
+}
+void Pedido::setId(char id) {
+	this->id = id;
+}
+
+// Setters
+
+void Pedido::setVolumen(int volumen) {
+	this->volumen = volumen;
+}
+void Pedido::setPrioridad(int prioridad) {
+	this->prioridad = prioridad;
+}
+void Pedido::setOrigenX(int origenX) {
+	this->origenX = origenX;
+}
+void Pedido::setOrigenY(int origenY) {
+	this->origenY = origenY;
+}
+void Pedido::setDestinoX(int destinoX) {
+	this->destinoX = destinoX;
+}
+void Pedido::setDestinoY(int destinoY) {
+	this->destinoY = destinoY;
+}
+
+// Métodos
--- Gestion de Entregas Urbanas/Pedido.h
+++ Gestion de Entregas Urbanas/Pedido.h
@@ -7,8 +7,36 @@ private:
 	char id;
 	int volumen;
 	int prioridad;
-
+	int origenX;
+	int origenY;
 	int destinoX;
 	int destinoY;
+
+public:
+
+	// Constructor
+	Pedido(char id, int volumen, int prioridad, int origenX, int origenY, int destinoX, int destinoY)
+		: id(id), volumen(volumen), prioridad(prioridad), origenX(origenX), origenY(origenY), destinoX(destinoX), destinoY(destinoY) {
+	}
+
+	// Getters
+	char getId();
+	int getVolumen();
+	int getPrioridad();
+	int getOrigenX();
+	int getOrigenY();
+	int getDestinoX();
+	int getDestinoY();
+
+	// Setters
+
+	void setId(char id);
+	void setVolumen(int volumen);
+	void setPrioridad(int prioridad);
+	void setOrigenX(int origenX);
+	void setOrigenY(int origenY);
+	void setDestinoX(int destinoX);
+	void setDestinoY(int destinoY);
+
 };
 #endif
